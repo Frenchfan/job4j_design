8e96a7393dae4705d18947735126867dc751e371
Frenchfan <sumkin.gpb@gmail.com>
2022-05-25 13:26:11 +0300

    Bounding Generics -additional samples

---
+++ src/main/java/ru/job4j/generics/Coords.java
@@ -0,0 +1,85 @@
+package ru.job4j.generics;
+
+public class Coords<T extends TwoD> {
+    T[] coords;
+
+    Coords(T[] o) {
+        coords = o;
+    }
+
+    /**
+     * Works with TwoD, ThreeD, FourD - as any of the class has at least X and Y coordinates
+     * @param c contains wild card
+     */
+    static void showXY(Coords<?> c) {
+        System.out.println("Координаты X и Y: ");
+        for (int i = 0; i < c.coords.length; i++) {
+            System.out.println(c.coords[i].x + " " + c.coords[i].y);
+            System.out.println();
+        }
+    }
+
+    static void showXYZ(Coords<? extends ThreeD> c) {
+        System.out.println("X, Y, Z coordinates: ");
+        for (int i = 0; i < c.coords.length; i++) {
+            System.out.println(c.coords[i].x + c.coords[i].y + c.coords[i].z);
+            System.out.println();
+        }
+    }
+}
+
+class BoundedWildCard {
+    static void showXY(Coords<?> c) {
+        System.out.println("Координаты X и Y: ");
+        for (int i = 0; i < c.coords.length; i++) {
+            System.out.println(c.coords[i].x + " " + c.coords[i].y);
+            System.out.println();
+        }
+    }
+
+    static void showXYZ(Coords<? extends ThreeD> c) {
+        System.out.println("X, Y, Z coordinates: ");
+        for (int i = 0; i < c.coords.length; i++) {
+            System.out.println(c.coords[i].x + " " + c.coords[i].y + " " + c.coords[i].z);
+            System.out.println();
+        }
+    }
+
+    static void showAll(Coords<? extends FourD> c) {
+        System.out.println("X, Y, Z, T coordinates: ");
+        for (int i = 0; i < c.coords.length; i++) {
+            System.out.println(c.coords[i].x + " " + c.coords[i].y + " " + c.coords[i].z + " " + c.coords[i].t);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        TwoD[] td = {
+                new TwoD(0, 0),
+                new TwoD(7, 9),
+                new TwoD(18, 4),
+                new TwoD(-1, -23)
+        };
+
+        Coords<TwoD> tdlocks = new Coords<>(td);
+
+        System.out.println("Содержимое объекта tdlocks.");
+        showXY(tdlocks);
+        //showXYZ(tdlocks);
+        //showAll(tdlocks);
+
+        FourD[] fd = {
+                new FourD(1, 2, 3, 4),
+                new FourD(6, 8, 14, 8),
+                new FourD(22, 9, 4, 9),
+                new FourD(3, -2, -23, 17)
+        };
+
+        Coords<FourD> fdlocs = new Coords<>(fd);
+        System.out.println("Содержимое объекта fdlocs. ");
+        //Здесь все верно
+        showXY(fdlocs);
+        showXYZ(fdlocs);
+        showAll(fdlocs);
+    }
+}
---
+++ src/main/java/ru/job4j/generics/TwoD.java
@@ -0,0 +1,28 @@
+package ru.job4j.generics;
+
+class TwoD {
+    int x, y;
+
+    TwoD(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
+
+class ThreeD extends TwoD {
+    int z;
+
+    ThreeD(int a, int b, int c) {
+        super(a, b);
+        z = c;
+    }
+}
+
+class FourD extends ThreeD {
+    int t;
+
+    FourD(int a, int b, int c, int d) {
+        super(a, b, c);
+        t = d;
+    }
+}
