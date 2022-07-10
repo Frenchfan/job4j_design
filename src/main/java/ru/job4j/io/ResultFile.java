c0cb0c44f3cbbfaf21cfd14f433056558e722754
Frenchfan <sumkin.gpb@gmail.com>
2022-07-10 03:21:17 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        int size = 9;
+        int[][] rsl = new int[size][size];
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    rsl[i][j] = (i + 1) * (j + 1);
+                    out.write(String.valueOf(rsl[i][j]).getBytes());
+                    out.write((" ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
