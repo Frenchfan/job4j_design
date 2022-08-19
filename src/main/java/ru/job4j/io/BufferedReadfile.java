a084c0605add1bd96524c462cf761759f2aa191d
Frenchfan <sumkin.gpb@gmail.com>
2022-08-20 02:33:40 +0300

    1. Читаем файл конфигурации [#858 #323968]

--- src/main/java/ru/job4j/io/BufferedReadfile.java
+++ src/main/java/ru/job4j/io/BufferedReadfile.java
@@ -1,10 +1,8 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class BuffferedReadfile {
+public class BufferedReadfile {
     public static void main(String[] args) {
         try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
             in.lines().forEach(System.out::println);
